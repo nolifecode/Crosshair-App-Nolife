a30f0a4a89ab9df7b464be6192ae103190023355
nolife.code <cysiekk@proton.me>
2026-01-02 00:23:03 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,16 +2,17 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLab
 from PyQt5.QtCore import Qt
 
 class MenuWindow(QWidget):
-    def __init__(self, settings, visual, spotify):
+    def __init__(self, settings, visual, spotify, profiles):
         super().__init__()
         self.settings = settings
         self.visual = visual
         self.spotify = spotify
+        self.profiles = profiles
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
         self.setAttribute(Qt.WA_TranslucentBackground)
-        self.setFixedSize(200, 280)
+        self.setFixedSize(200, 320)
         self.container = QWidget(self)
-        self.container.setGeometry(0, 0, 200, 280)
+        self.container.setGeometry(0, 0, 200, 320)
         self.setup_ui()
         self.update_theme("#888")
         self.move(QApplication.primaryScreen().geometry().width() - self.width() - 20, 20)
@@ -20,12 +21,14 @@ class MenuWindow(QWidget):
         layout = QVBoxLayout(self.container)
         layout.setContentsMargins(15, 20, 15, 10)
         layout.addWidget(QLabel("Nolife.code | Crosshair"))
-        layout.addSpacing(20)
+        layout.addSpacing(10)
 
-        self.btn_cross = QPushButton("> SHOW CROSSHAIR [F1]")
-        self.btn_menu = QPushButton("> SHOW MENU [INS]")
-        layout.addWidget(self.btn_cross)
-        layout.addWidget(self.btn_menu)
+        layout.addWidget(QPushButton("> TOGGLE CROSSHAIR [F1]"))
+        
+        self.btn_spotify = QPushButton("> SPOTIFY WIDGET: ON")
+        self.btn_spotify.clicked.connect(self.toggle_spotify_active)
+        layout.addWidget(self.btn_spotify)
+        
         layout.addStretch()
 
         layout.addWidget(QLabel("> CHANGE THEME"))
@@ -37,12 +40,22 @@ class MenuWindow(QWidget):
             btn.clicked.connect(lambda checked, col=c: self.update_theme(col))
             color_layout.addWidget(btn)
         layout.addLayout(color_layout)
+        
         layout.addSpacing(10)
+        btn_exit = QPushButton("[ EXIT ]")
+        btn_exit.setObjectName("ExitBtn")
+        btn_exit.clicked.connect(lambda: exit())
+        layout.addWidget(btn_exit)
 
-        self.btn_exit = QPushButton("[ EXIT ]")
-        self.btn_exit.setObjectName("ExitBtn")
-        self.btn_exit.clicked.connect(lambda: exit())
-        layout.addWidget(self.btn_exit)
+    def toggle_spotify_active(self):
+        if self.spotify.isVisible():
+            self.spotify.hide()
+            self.spotify.timer.stop()
+            self.btn_spotify.setText("> SPOTIFY WIDGET: OFF")
+        else:
+            self.spotify.show()
+            self.spotify.timer.start(2000)
+            self.btn_spotify.setText("> SPOTIFY WIDGET: ON")
 
     def update_theme(self, color):
         style = f"""
@@ -50,6 +63,8 @@ class MenuWindow(QWidget):
             QLabel {{ color: #444; font-family: 'Consolas'; font-size: 10px; border: none; }}
             QPushButton {{ background-color: #111; color: #888; border: 1px solid #222; padding: 5px; font-family: 'Consolas'; font-size: 11px; }}
             QPushButton:hover {{ background-color: #181818; color: {color}; }}
+            QLineEdit {{ background-color: #111; color: #888; border: 1px solid #222; font-family: 'Consolas'; font-size: 10px; }}
+            QScrollArea {{ background: transparent; }}
             #ExitBtn {{ color: #444; text-align: center; background: transparent; border: none; }}
             #ExitBtn:hover {{ color: #AA0000; }}
         """
@@ -57,3 +72,4 @@ class MenuWindow(QWidget):
         self.settings.container.setStyleSheet(style)
         self.visual.container.setStyleSheet(style)
         self.spotify.container.setStyleSheet(style)
+        self.profiles.container.setStyleSheet(style)
